6805e4affc556c00d7646bdcbb6db17d8f9f14ac
MESCUIS Developer Solutions Planning Development Devision <52739498+mescius-ds-dev@users.noreply.github.com>
2023-12-21 15:25:55 +0900

    Add files via upload

---
+++ CS/SpreadWinPerformanceCS/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace SpreadWinPerformanceCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fpSpread1 = new FarPoint.Win.Spread.FpSpread();
+            this.fpSpread1_Sheet1 = new FarPoint.Win.Spread.SheetView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1_Sheet1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fpSpread1
+            // 
+            this.fpSpread1.AccessibleDescription = "";
+            this.fpSpread1.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 11F);
+            this.fpSpread1.Location = new System.Drawing.Point(12, 12);
+            this.fpSpread1.Name = "fpSpread1";
+            this.fpSpread1.Sheets.AddRange(new FarPoint.Win.Spread.SheetView[] {
+            this.fpSpread1_Sheet1});
+            this.fpSpread1.Size = new System.Drawing.Size(600, 426);
+            this.fpSpread1.TabIndex = 0;
+            // 
+            // fpSpread1_Sheet1
+            // 
+            this.fpSpread1_Sheet1.Reset();
+            this.fpSpread1_Sheet1.SheetName = "Sheet1";
+            // Formulas and custom names must be loaded with R1C1 reference style
+            this.fpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1;
+            this.fpSpread1_Sheet1.ColumnFooter.DefaultStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.ColumnFooter.DefaultStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.ColumnFooter.DefaultStyle.Locked = false;
+            this.fpSpread1_Sheet1.ColumnFooterSheetCornerStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.ColumnFooterSheetCornerStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.ColumnFooterSheetCornerStyle.Locked = false;
+            this.fpSpread1_Sheet1.ColumnHeader.DefaultStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.ColumnHeader.DefaultStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.ColumnHeader.DefaultStyle.Locked = false;
+            this.fpSpread1_Sheet1.DefaultStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.DefaultStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.DefaultStyle.Locked = false;
+            this.fpSpread1_Sheet1.FilterBar.DefaultStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.FilterBar.DefaultStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.FilterBar.DefaultStyle.Locked = false;
+            this.fpSpread1_Sheet1.FilterBarHeaderStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.FilterBarHeaderStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.FilterBarHeaderStyle.Locked = false;
+            this.fpSpread1_Sheet1.RowHeader.DefaultStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.RowHeader.DefaultStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.RowHeader.DefaultStyle.Locked = false;
+            this.fpSpread1_Sheet1.Rows.Default.Height = 18F;
+            this.fpSpread1_Sheet1.SheetCornerStyle.BackColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.SheetCornerStyle.ForeColor = System.Drawing.Color.Empty;
+            this.fpSpread1_Sheet1.SheetCornerStyle.Locked = false;
+            this.fpSpread1_Sheet1.SheetCornerStyle.VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
+            this.fpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(618, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(170, 180);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(618, 304);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(170, 134);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "スタイルの設定";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "フィルタの実行";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "フィルタリストの表示";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Excelファイルのインポート";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(618, 198);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(158, 24);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "描画の制御なし";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(6, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(158, 24);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "描画の一時停止";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(6, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(158, 24);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "描画とイベントの一時停止";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.fpSpread1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SPREADのパフォーマンスとメモリ消費";
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1_Sheet1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FarPoint.Win.Spread.FpSpread fpSpread1;
+        private FarPoint.Win.Spread.SheetView fpSpread1_Sheet1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ CS/SpreadWinPerformanceCS/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using FarPoint.Excel;
+using FarPoint.Win.Spread;
+
+namespace SpreadWinPerformanceCS
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder Log { get; set; }
+        private DateTime Time1 { get; set; }
+        private DateTime Time2 { get; set; }
+        private Int64 Memory1 { get; set; }
+        private Int64 Memory2 { get; set; }
+        private String TestItem { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // SPREADの初期化
+            InitializeSpread(this);
+
+            // ログの開始
+            Log = new StringBuilder();
+
+            // イベントハンドラの設定
+            button1.Click += button_Click;
+            button2.Click += button_Click;
+            button3.Click += button_Click;
+            button4.Click += button_Click;
+            this.Deactivate += Form1_Deactivate;
+            this.FormClosing += Form1_FormClosing;
+
+            this.Text = Text + " - " + System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
+        }
+
+        private FpSpread InitializeSpread(Form form)
+        {
+            // 新しいSPREADの生成と設定
+            foreach (Control c in form.Controls)
+            {
+                if (c is FpSpread)
+                {
+                    //// SPREADの生成：互換機能を使用する場合
+                    //FpSpread spread = new FpSpread();
+
+                    // SPREADの生成：新機能を使用する場合
+                    FpSpread spread = new FpSpread(LegacyBehaviors.None);
+
+                    // SPREADの設定
+                    spread.Location = c.Location;
+                    spread.Size = c.Size;
+                    spread.Sheets.Add(new SheetView());
+
+                    // シートの設定
+                    var sheet = spread.ActiveSheet;
+                    sheet.ColumnCount = 50;
+                    sheet.RowCount = 10000;
+                    sheet.AutoFilterMode = AutoFilterMode.EnhancedContextMenu;
+                    sheet.AutoSortEnhancedContextMenu = true;
+                    sheet.Columns[0].AllowAutoFilter = true;
+                    sheet.Columns[0].AllowAutoSort = true;
+                    sheet.RowHeader.Columns[0].Width = 60;
+                    for (var i = 0; i < sheet.RowCount; i++)
+                    {
+                        sheet.SetValue(i, 0, $"A{i % 5000}");
+                        for (var j = 1; j < sheet.ColumnCount; j++)
+                        {
+                            sheet.SetValue(i, j, $"R{i}C{j}");
+                        }
+                    }
+
+                    // Formの設定
+                    form.Controls.Remove(c);
+                    form.Controls.Add(spread);
+                    form.Refresh();
+
+                    // 戻り値の設定
+                    return spread;
+                }
+            }
+
+            return null;
+        }
+
+        private void GetInitialInfo()
+        {
+            // 処理開始時のメモリ使用量と時刻の取得
+            GC.Collect();
+            Memory1 = Environment.WorkingSet;
+            Time1 = DateTime.Now;
+        }
+
+        private String GetResult(object sender, FpSpread spread, bool measure)
+        {
+            // 処理完了時のメモリ使用量と時刻の取得
+            Time2 = DateTime.Now;
+            GC.Collect();
+            Memory2 = Environment.WorkingSet;
+
+            // 基本情報の取得
+            var sb = new StringBuilder();
+            sb.AppendLine($"[{(sender as Control).Text}]");
+            sb.AppendLine($"Version = {spread.ProductVersion}");
+            sb.AppendLine($"LegacyBehaviors = {spread.GetType().GetProperty("LegacyBehaviors").GetValue(spread)}");
+            sb.AppendLine();
+
+            // 計測結果の取得
+            if (measure)
+            {
+                // 処理時間
+                sb.AppendLine($"開始時刻 = {Time1:HH:mm:ss}");
+                sb.AppendLine($"終了時刻 = {Time2:HH:mm:ss}");
+                sb.AppendLine("-------------------------");
+                sb.AppendLine($"処理時間 = {Time2.Subtract(Time1).TotalMilliseconds:#,##0} ミリ秒");
+                sb.AppendLine();
+
+                // メモリ使用量
+                sb.AppendLine($"処理前 = {Memory1:#,##0} Byte");
+                sb.AppendLine($"処理後 = {Memory2:#,##0} Byte");
+                sb.AppendLine("-------------------------");
+                sb.AppendLine($"増加量 = {Memory2 - Memory1:#,##0} Byte");
+
+                // ログの記録
+                Log.AppendLine(sb.ToString());
+            }
+
+            // 戻り値の設定
+            return sb.ToString();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            // テスト項目の保存
+            TestItem = (sender as Control).Name;
+
+            // マウスカーソルの変更
+            this.Cursor = Cursors.WaitCursor;
+
+            // テキストボックスの初期化
+            textBox1.Text = string.Empty;
+            textBox1.Refresh();
+
+            // SPREADの生成
+            var spread = InitializeSpread(this);
+
+            // 処理前の描画制御
+            if (radioButton1.Checked)
+            {
+                // 描画の一時停止
+                spread.SuspendLayout();
+
+                // 描画とイベントの一時停止
+                spread.AsWorkbook().WorkbookSet.BeginUpdate();
+            }
+            else if (radioButton2.Checked)
+            {
+                // 描画の一時停止
+                spread.SuspendLayout();
+            }
+            else if (radioButton3.Checked)
+            {
+                // 描画の制御なし
+            }
+
+            // 処理の実行
+            //
+            switch ((sender as Control).Name)
+            {
+                case "button1":
+                    // 処理開始時のメモリ使用量と時刻の取得
+                    GetInitialInfo();
+
+                    // Excelファイルのインポート
+                    try
+                    {
+                        spread.OpenExcel("PerformanceTest_withStyle.xlsx", ExcelOpenFlags.TruncateEmptyRowsAndColumns);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"テスト：{ex.Message}");
+                    }
+                    break;
+                case "button2":
+                    // フィルタリストの表示
+
+                    #region "参考：ver.12の機能を使った計測方法"
+                    //spread.BeforeShowContextMenu += (s, ea) =>
+                    //{
+                    //    // マウスカーソルの変更
+                    //    this.Cursor = Cursors.WaitCursor;
+
+                    //    // 処理開始時のメモリ使用量と時刻の取得
+                    //    GetInitialInfo();
+
+                    //    // 処理完了時のメモリ使用量と時刻の取得
+                    //    ea.ContextMenu.Opened += (s1, ea1) =>
+                    //    {
+                    //        textBox1.Text = GetResult(sender, spread, true);
+
+                    //        // マウスカーソルの復元
+                    //        this.Cursor = Cursors.Default;
+                    //    };
+                    //};
+                    #endregion
+
+                    spread.MouseDown += (s, ea) =>
+                    {
+                        var ht = spread.HitTest(ea.X, ea.Y);
+                        if (ht.Type == HitTestType.ColumnHeader && ht.HeaderInfo.Column == 0)
+                        {
+                            // マウスカーソルの変更
+                            this.Cursor = Cursors.WaitCursor;
+
+                            // 処理開始時のメモリ使用量と時刻の取得
+                            GetInitialInfo();
+                        }
+                    };
+
+                    // 開始処理のキャンセル
+                    spread.CellClick += (s, ea) =>
+                    {
+                        if (ea.ColumnHeader && ea.Column == 0)
+                        {
+                            Time1 = DateTime.MinValue;
+                            Memory1 = 0;
+                            this.Cursor = Cursors.Default;
+                        }
+                    };
+                    break;
+                case "button3":
+                    // フィルタの実行
+
+                    #region "参考：コードによるフィルタの実行"
+                    //// 処理開始時のメモリ使用量と時刻の取得
+                    //GetInitialInfo();
+
+                    ////// SheetViewの機能を使う方法
+                    ////spread.ActiveSheet.AutoFilterColumn(0, "A100", 0);
+
+                    //// IWorkSheetの機能を使う方法：高速です！
+                    //var wsheet = spread.ActiveSheet.AsWorksheet();
+                    //wsheet.InsertRows(0, 1);
+                    //wsheet.Cells[0, 0, wsheet.RowCount - 1, wsheet.ColumnCount - 1].AutoFilter();
+                    //wsheet.Cells[0, 0, wsheet.RowCount - 1, wsheet.ColumnCount - 1].AutoFilter(0, criteria1: "=A100");
+                    #endregion
+
+                    spread.AutoFilteringColumn += (s, ea) =>
+                    {
+                        // マウスカーソルの変更
+                        this.Cursor = Cursors.WaitCursor;
+
+                        // 処理開始時のメモリ使用量と時刻の取得
+                        GetInitialInfo();
+                    };
+                    spread.AutoFilteredColumn += (s, ea) =>
+                    {
+                        // 処理完了時のメモリ使用量と時刻の取得
+                        textBox1.Text = GetResult(sender, spread, true);
+
+                        // マウスカーソルの復元
+                        this.Cursor = Cursors.Default;
+                    };
+                    break;
+                case "button4":
+                    // 処理開始時のメモリ使用量と時刻の取得
+                    GetInitialInfo();
+
+                    // スタイルの設定
+
+                    #region "参考：IWorkSheetの機能を使う方法：高速です！"
+                    // この手法はLegacyBehaviorsのStyleフラグがオフのときにのみ有効です
+                    var wsheet = spread.ActiveSheet.AsWorksheet();
+                    var backColor = GrapeCity.Spreadsheet.Color.FromArgb(Color.LavenderBlush.ToArgb());
+                    var foreColor = GrapeCity.Spreadsheet.Color.FromArgb(Color.Blue.ToArgb());
+                    for (var i = 0; i < wsheet.RowCount; i++)
+                    {
+                        for (var j = 0; j < wsheet.ColumnCount; j++)
+                        {
+                            wsheet.Cells[i, j].Interior.Color = backColor;
+                            wsheet.Cells[i, j].Font.Color = foreColor;
+                        }
+                    }
+                    #endregion
+
+                    //var sheet = spread.ActiveSheet;
+                    //for (var i = 0; i < sheet.RowCount; i++)
+                    //{
+                    //    for (var j = 0; j < sheet.ColumnCount; j++)
+                    //    {
+                    //        sheet.Cells[i, j].BackColor = Color.LavenderBlush;
+                    //        sheet.Cells[i, j].ForeColor = Color.Blue;
+                    //    }
+                    //}
+                    break;
+            }
+
+            // 処理後の描画制御
+            if (radioButton1.Checked)
+            {
+                // 描画とイベントの一時停止の解除
+                spread.AsWorkbook().WorkbookSet.EndUpdate();
+
+                // 描画の一時停止の解除
+                spread.ResumeLayout();
+            }
+            else if (radioButton2.Checked)
+            {
+                // 描画の一時停止の解除
+                spread.ResumeLayout();
+            }
+            else if (radioButton3.Checked)
+            {
+                // 描画の制御なし
+            }
+
+            // 情報の取得と表示
+            if ((sender as Control).Name == "button2")
+            {
+                textBox1.Text = GetResult(sender, spread, false);
+                spread.ActiveSheet.TitleInfo.Text = "第１列の列ヘッダ内のドロップダウンボタンをクリックしてください。";
+                spread.ActiveSheet.TitleInfo.ForeColor = Color.Red;
+                spread.ActiveSheet.TitleInfo.Visible = true;
+            }
+            else if ((sender as Control).Name == "button3")
+            {
+                textBox1.Text = GetResult(sender, spread, false);
+                spread.ActiveSheet.TitleInfo.Text = "第１列でフィルタ操作を行って「A0」のチェックを外してOKボタンをクリックしてください。";
+                spread.ActiveSheet.TitleInfo.ForeColor = Color.Red;
+                spread.ActiveSheet.TitleInfo.Visible = true;
+            }
+            else
+            {
+                textBox1.Text = GetResult(sender, spread, true);
+            }
+
+            // マウスカーソルの復元
+            this.Cursor = Cursors.Default;
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            // 処理完了時のメモリ使用量と時刻の取得
+            if (TestItem == "button2" && Time1 != DateTime.MinValue)
+            {
+                foreach (Control c in (sender as Form).Controls)
+                {
+                    if (c is FpSpread)
+                    {
+                        textBox1.Text = GetResult(button2, c as FpSpread, true);
+
+                        // マウスカーソルの復元
+                        this.Cursor = Cursors.Default;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // ログの出力
+            using (FileStream fs = File.OpenWrite($"v12_{DateTime.Now:yyyyMMddHHmmss}.log"))
+            {
+                var info = new UTF8Encoding(true).GetBytes(Log.ToString());
+                fs.Write(info, 0, info.Length);
+            }
+        }
+    }
+}
