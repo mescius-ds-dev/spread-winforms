e60bf4f9b57ac0e7479e7e72d8e42e9237e9c674
MESCUIS Developer Solutions Planning Development Devision <52739498+mescius-ds-dev@users.noreply.github.com>
2024-02-21 10:14:21 +0900

    Add files via upload

---
+++ VB/SpreadWinDemo/databind/tablebinding.vb
@@ -0,0 +1,26 @@
+﻿Public Class tablebinding
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        ' ワークブックの設定
+        InitWorkbook(FpSpread1.AsWorkbook())
+    End Sub
+
+    Private Sub InitWorkbook(ByVal workbook As GrapeCity.Spreadsheet.IWorkbook)
+        ' フォントの設定
+        Dim normalStyle As GrapeCity.Spreadsheet.IStyle = workbook.Styles(GrapeCity.Spreadsheet.BuiltInStyle.Normal)
+        normalStyle.Font.Name = "メイリオ"
+        normalStyle.Font.Size = 9
+
+        ' テストデータの設定
+        Dim ds As New DataSet()
+        ds.ReadXml(Me.GetType().Assembly.GetManifestResourceStream(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name & ".data.xml"))
+
+        ' テーブルの追加
+        Dim table As GrapeCity.Spreadsheet.ITable = workbook.ActiveSheet.Tables.Add("B2:J14", GrapeCity.Spreadsheet.YesNoGuess.Yes)
+        ' テーブルと連結
+        table.DataSource = ds
+    End Sub
+End Class
