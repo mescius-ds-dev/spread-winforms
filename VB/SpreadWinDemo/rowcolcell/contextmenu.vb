0ef1bfa47b2694fdceb6b9676131d03d141e0340
MESCUIS Developer Solutions Planning Development Devision <52739498+mescius-ds-dev@users.noreply.github.com>
2025-05-15 16:18:22 +0900

    Update for SP2.

---
+++ VB/SpreadWinDemo/rowcolcell/contextmenu.vb
@@ -0,0 +1,16 @@
+﻿Imports FarPoint.Win.Spread
+
+Public Class contextmenu
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        ' シートの設定
+        InitSpreadStyles(FpSpread1.Sheets(0))
+    End Sub
+
+    Private Sub InitSpreadStyles(sheet As FarPoint.Win.Spread.SheetView)
+        FpSpread1.ContextMenuStrip = New SpreadContextMenuStrip()
+    End Sub
+End Class
