22d1c14ed1518b62611ad6f2270efd18a85a554c
MESCUIS Developer Solutions Planning Development Devision <52739498+mescius-ds-dev@users.noreply.github.com>
2025-05-15 16:02:22 +0900

    Update for SP2.

---
+++ VB/SpreadWinDemo/databind/tableautoupdate.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class tableautoupdate
+    Inherits SpreadWinDemo.DemoBase
+
+    'Form は、コンポーネント一覧に後処理を実行するために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SplitContainer3 = New System.Windows.Forms.SplitContainer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FpSpread1 = New FarPoint.Win.Spread.FpSpread()
+        Me.FpSpread1_Sheet1 = New FarPoint.Win.Spread.SheetView()
+        Me.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.SplitContainer3.Panel1.SuspendLayout()
+        Me.SplitContainer3.Panel2.SuspendLayout()
+        Me.SplitContainer3.SuspendLayout()
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.SplitContainer1)
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SplitContainer2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.FpSpread1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(708, 400)
+        Me.SplitContainer1.SplitterDistance = 30
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.CheckBox1)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.SplitContainer3)
+        Me.SplitContainer2.Size = New System.Drawing.Size(708, 30)
+        Me.SplitContainer2.SplitterDistance = 260
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CheckBox1.Location = New System.Drawing.Point(0, 0)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(260, 30)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "自動更新を有効"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'SplitContainer3
+        '
+        Me.SplitContainer3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer3.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer3.Name = "SplitContainer3"
+        '
+        'SplitContainer3.Panel1
+        '
+        Me.SplitContainer3.Panel1.Controls.Add(Me.Button1)
+        '
+        'SplitContainer3.Panel2
+        '
+        Me.SplitContainer3.Size = New System.Drawing.Size(444, 30)
+        Me.SplitContainer3.SplitterDistance = 220
+        Me.SplitContainer3.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(220, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "データソースの変更"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FpSpread1
+        '
+        Me.FpSpread1.AccessibleDescription = ""
+        Me.FpSpread1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FpSpread1.Location = New System.Drawing.Point(0, 0)
+        Me.FpSpread1.Name = "FpSpread1"
+        Me.FpSpread1.Sheets.AddRange(New FarPoint.Win.Spread.SheetView() {Me.FpSpread1_Sheet1})
+        Me.FpSpread1.Size = New System.Drawing.Size(708, 366)
+        Me.FpSpread1.TabIndex = 0
+        '
+        'FpSpread1_Sheet1
+        '
+        Me.FpSpread1_Sheet1.Reset()
+        Me.FpSpread1_Sheet1.SheetName = "Sheet1"
+        '
+        'touchkeyboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 18.0!)
+        Me.Description = "フィルタで特定のアイテムを除外した状態で手動でセルを編集したりデータソースを変更した時に、変更後の値に自動的にフィルタを適用しテーブルの状態を維持します。"
+        Me.Name = "touchkeyboard"
+        Me.Title = "テーブルフィルタの自動更新"
+        Me.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.PerformLayout()
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.SplitContainer3.Panel1.ResumeLayout(False)
+        Me.SplitContainer3.Panel2.ResumeLayout(False)
+        Me.SplitContainer3.ResumeLayout(False)
+        CType(Me.FpSpread1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FpSpread1_Sheet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents SplitContainer2 As System.Windows.Forms.SplitContainer
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents SplitContainer3 As System.Windows.Forms.SplitContainer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FpSpread1 As FarPoint.Win.Spread.FpSpread
+    Friend WithEvents FpSpread1_Sheet1 As FarPoint.Win.Spread.SheetView
+
+End Class
---
+++ VB/SpreadWinDemo/databind/tableautoupdate.vb
@@ -0,0 +1,49 @@
+﻿Public Class tableautoupdate
+
+    Public Sub New()
+
+        InitializeComponent()
+
+        ' ワークブックの設定
+        InitWorkbook(FpSpread1.AsWorkbook())
+
+        CheckBox1.Checked = True
+    End Sub
+
+    Private ds As DataSet
+    Private Sub InitWorkbook(workbook As GrapeCity.Spreadsheet.IWorkbook)
+        ' フォントの設定
+        Dim normalStyle As GrapeCity.Spreadsheet.IStyle = workbook.Styles(GrapeCity.Spreadsheet.BuiltInStyle.Normal)
+        normalStyle.Font.Name = "メイリオ"
+        normalStyle.Font.Size = 9
+
+        ' テーブルの自動更新を有効
+        workbook.ActiveSheet.AutoUpdateFilter = True
+
+        ' テストデータの設定
+        ds = New DataSet()
+        ds.ReadXml(Me.GetType().Assembly.GetManifestResourceStream(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name & ".data.xml"))
+
+        ' テーブルの追加
+        Dim table As GrapeCity.Spreadsheet.ITable = workbook.ActiveSheet.Tables.Add("B2:J14", GrapeCity.Spreadsheet.YesNoGuess.Yes)
+        ' テーブルと連結
+        table.DataSource = ds
+    End Sub
+
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        ' SPREADの準備が完了していない場合を除外
+        If FpSpread1.Sheets.Count = 0 Then Return
+
+        Dim workbook As GrapeCity.Spreadsheet.IWorkbook = FpSpread1.AsWorkbook()
+        Dim table As GrapeCity.Spreadsheet.ITable = workbook.ActiveSheet.Tables(0)
+
+        ' テーブルフィルタの自動更新の切り替え
+        table.AutoFilter.AutoUpdate = CheckBox1.Checked
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
+        ' データソースの変更
+        ds.Tables(0).Rows(6)(0) = "1001"
+    End Sub
+End Class
