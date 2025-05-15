52ab98a5620045ad9f0876a0b1a59c0b0482afa6
MESCUIS Developer Solutions Planning Development Devision <52739498+mescius-ds-dev@users.noreply.github.com>
2025-05-15 16:45:53 +0900

    Update for SP2.

---
+++ CS/SpreadWinDemoCS/databind/tableautoupdate.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SpreadWinDemo.databind
+{
+    partial class tableautoupdate
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tableautoupdate));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fpSpread1 = new FarPoint.Win.Spread.FpSpread(FarPoint.Win.Spread.LegacyBehaviors.None, ((object)(resources.GetObject("splitContainer1.Panel2.Controls"))));
+            this.fpSpread1_Sheet1 = this.fpSpread1.GetSheet(0);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.fpSpread1);
+            this.splitContainer1.Size = new System.Drawing.Size(708, 400);
+            this.splitContainer1.SplitterDistance = 30;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.checkBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(708, 30);
+            this.splitContainer2.SplitterDistance = 233;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(233, 30);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "自動更新を有効";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.button1);
+            this.splitContainer3.Size = new System.Drawing.Size(471, 30);
+            this.splitContainer3.SplitterDistance = 231;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "データソースの変更";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // fpSpread1
+            // 
+            this.fpSpread1.AccessibleDescription = "";
+            this.fpSpread1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fpSpread1.Location = new System.Drawing.Point(0, 0);
+            this.fpSpread1.Name = "fpSpread1";
+            this.fpSpread1.Size = new System.Drawing.Size(708, 366);
+            this.fpSpread1.TabIndex = 0;
+            // 
+            // tableautoupdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.Description = "フィルタで特定のアイテムを除外した状態で手動でセルを編集したりデータソースを変更した時に、変更後の値に自動的にフィルタを適用しテーブルの状態を維持します。";
+            this.Name = "tableautoupdate";
+            this.Title = "テーブルフィルタの自動更新";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Button button1;
+        private FarPoint.Win.Spread.FpSpread fpSpread1;
+        private FarPoint.Win.Spread.SheetView fpSpread1_Sheet1;
+
+    }
+}
---
+++ CS/SpreadWinDemoCS/databind/tableautoupdate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SpreadWinDemo.databind
+{
+    public partial class tableautoupdate : SpreadWinDemo.DemoBase
+    {
+        public tableautoupdate()
+        {
+            InitializeComponent();
+
+            // ワークブックの設定
+            InitWorkbook(fpSpread1.AsWorkbook());
+
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        DataSet ds;
+        private void InitWorkbook(GrapeCity.Spreadsheet.IWorkbook workbook)
+        {
+            // フォントの設定
+            GrapeCity.Spreadsheet.IStyle normalStyle = workbook.Styles[GrapeCity.Spreadsheet.BuiltInStyle.Normal];
+            normalStyle.Font.Name = "メイリオ";
+            normalStyle.Font.Size = 9;
+
+            // テストデータの設定
+            ds = new DataSet();
+            ds.ReadXml(this.GetType().Assembly.GetManifestResourceStream(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + ".SampleData.data.xml"));
+
+            // テーブルの追加
+            fpSpread1.AsWorkbook().ActiveSheet.AutoUpdateFilter = true;
+            GrapeCity.Spreadsheet.ITable table = workbook.ActiveSheet.Tables.Add("B2:J14", GrapeCity.Spreadsheet.YesNoGuess.Yes);
+            // テーブルと連結
+            table.DataSource = ds;
+            checkBox1.Checked = workbook.ActiveSheet.Tables[0].AutoFilter.AutoUpdate;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // SPREADの準備が完了していない場合を除外
+            if (fpSpread1.Sheets.Count == 0) return;
+
+            GrapeCity.Spreadsheet.IWorkbook workbook = fpSpread1.AsWorkbook();
+            GrapeCity.Spreadsheet.ITable table = workbook.ActiveSheet.Tables[0];
+
+            // テーブルフィルタの自動更新の切り替え
+            table.AutoFilter.AutoUpdate = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // データソースの変更
+            ds.Tables[0].Rows[6][0] = "1001";
+        }
+    }
+}
